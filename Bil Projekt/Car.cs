8f6ded052f3014c81e15b440ad8360ebf7533fee
Carl-Emil Holm Olsen <carl2291@elev.tec.dk>
2022-05-02 12:14:31 +0200

    Add project files.

---
+++ Bil Projekt/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bil_Projekt
+{
+    internal class Car
+    {
+        private string _Brand;
+        private string _Model;
+        private MyColorEnum _Color;
+        private int _Topspeed;
+        private double _Weight;
+        private int _HP;
+
+        public Car() { }
+
+        public Car (string brand, string model, MyColorEnum color)
+        {
+            _Brand = brand;
+            _Model = model;
+            _Color = color;
+        }
+
+        public Car (string brand, string model, MyColorEnum color, int top, int weight, int hp)
+        {
+            _Brand = brand;
+            _Model = model;
+            _Color = color;
+            _Topspeed = top;
+            _Weight = weight;
+            _HP = hp;
+
+        }
+        
+        public string brand 
+        {
+            get { return _Brand; }
+            set { _Brand = value; }
+        }
+
+        public string model
+        {
+            get { return _Model; }
+            set { _Model = value; }
+        }
+
+        public MyColorEnum color
+        {
+            get { return _Color;}
+            set { _Color = value;}
+        }
+            
+
+        public int topspeed
+        {
+            get { return _Topspeed; }
+            set { _Topspeed = value; }
+        }
+
+        public double weight
+        {
+            get { return _Weight; }
+            set { _Weight = value; }
+        }
+
+        public int hp
+        {
+            get { return _HP; }
+            set { _HP = value; }
+        }
+
+        public double GetAcceleration()
+        {
+           return _Weight / _HP;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Brand : {0,-8}, Model: {1,-8}, Color: {2,-8}, TopSpeed: {3,-8}, Weight: {4,-8}, HP: {5,-8}", this.brand, this.model, this.color, this.topspeed, this.weight, this.hp, GetAcceleration());
+        }
+    }
+}
---
+++ Bil Projekt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bil_Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mercedes", " AMG", MyColorEnum.black, 450, 1800, 330);
+            Console.WriteLine(car1.ToString());
+            Car car2 = new Car("tesla", "Model 3", MyColorEnum.Blue);
+            car2.topspeed = 500;
+            car2.weight = 2000;
+            car2.hp = 700;
+            Console.WriteLine(car2.ToString());
+            Car car3 = new Car();
+            car3.brand = "Toyota";
+            car3.model = "Corolla";
+            car3.color = MyColorEnum.Red;
+            car3.topspeed = 280;
+            car3.weight = 1300;
+            car3.hp = 110;
+            Console.WriteLine(car3.ToString());
+            Console.ReadLine();
+        }
+    }
+}
